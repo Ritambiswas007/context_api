e6a23bd56a2700288e11fb130b2841d80dfeafd8
Ritambiswas007 <114689483+Ritambiswas007@users.noreply.github.com>
2023-12-27 16:39:39 +0530

    first commit

---
+++ api_react/src/App.jsx
@@ -0,0 +1,15 @@
+import UserContextProvider from "./context/UserContextProvider"
+
+
+function App() {
+  
+
+  return (
+    <UserContextProvider>
+    <h1>react with chai </h1>
+      
+    </UserContextProvider>
+  )
+}
+
+export default App
